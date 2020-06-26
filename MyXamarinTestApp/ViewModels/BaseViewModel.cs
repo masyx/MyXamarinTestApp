93c862c0edff56eb244ff1eff70ce35e7adbf81d
Sergey <rfmatveev@gmail.com>
2020-06-26 12:32:47 -0500

    Project created

---
+++ MyXamarinTestApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MyXamarinTestApp.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MyXamarinTestApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MyXamarinTestApp.Views;
+using Xamarin.Forms;
+
+namespace MyXamarinTestApp.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel()
+        {
+
+        }
+        public MainPageViewModel(INavigation navigation)
+        {
+            this.Navigation = navigation;
+            
+        }
+
+        public INavigation Navigation { get; set; }
+
+        public Command NavigateToTimerCommand =>
+            new Command(async() =>
+            {
+                await Navigation.PushAsync(new TimerPage());
+            });
+
+    }
+}
